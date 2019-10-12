b618bfd5a0c0cd39b1005f8e83b3ae90cbb8acd1
周永强 <hzzhouyongqiang@corp.netease.com>
2019-10-12 18:10:15 +0800

    Updates

---
+++ precache-manifest.d305e8747eea1f33eaef16df7acceb0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28a15cd309e1e5e403fac6690d14132",
+    "url": "/mineral/index.html"
+  },
+  {
+    "revision": "0f0b825bd724e11f5dd1",
+    "url": "/mineral/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20bdc545d7a93a29d98a",
+    "url": "/mineral/static/js/2.40351770.chunk.js"
+  },
+  {
+    "revision": "0f0b825bd724e11f5dd1",
+    "url": "/mineral/static/js/main.32f18546.chunk.js"
+  },
+  {
+    "revision": "6b132f773a737d2d6c81",
+    "url": "/mineral/static/js/runtime-main.066e75fc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/mineral/static/media/logo.25bf045c.svg"
+  }
+]);
